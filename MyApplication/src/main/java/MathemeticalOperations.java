e969db36f0ea1e7c4307cd95e1c4799041a29489
user <user@DESKTOP-GOLUM47>
2020-07-18 01:11:42 -0700

    committing the changes

---
+++ MyApplication/src/main/java/MathemeticalOperations.java
@@ -0,0 +1,17 @@
+
+public class MathemeticalOperations {
+
+	public static void print(int array[]) {
+
+		if (array.length == 0) {
+			System.out.println("array size is 0");
+		} else {
+			System.out.print("elements fo array are=");
+			for (int ele : array) {
+				System.out.print(ele + " ");
+			}
+		}
+
+	}
+
+}
---
+++ MyApplication/src/main/java/MyApplicationTest.java
@@ -0,0 +1,13 @@
+
+public class MyApplicationTest {
+
+	public static void main(String[] args) {
+
+		int array[] = { 1, 2, 3, 4, 5 };
+
+		// MathemeticalOperations operations = new MathemeticalOperations();
+		MathemeticalOperations.print(array);
+
+	}
+
+}
